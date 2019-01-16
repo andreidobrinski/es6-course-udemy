12efa5355ea846824d255911ab12f93f0800817c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-01-16 14:56:31 -0500

    add notes about reassigning const

--- app/index.js
+++ app/index.js
@@ -6,3 +6,12 @@ console.log(a);
   let a = 'goodbye';
   console.log('inside scope: ', a);
 }
+
+// can't reassign const
+//
+// const array = [1, 2, 3];
+// array.push(4);
+// console.log(array); // [1, 2, 3, 4]
+//
+// array = [3];
+// // syntax error
